cac14771bed76ac9d68c21987d30ca665140253d
Bruno Pinto <brpinto@student.42.fr>
2026-09-17 20:41:08 +0200

    add launch menu + handle pause

---
+++ Scripts/main.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(_delta: float) -> void:
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event:
+		if event.is_action_pressed("ui_select"):
+			toggle_pause()
+
+func toggle_pause():
+	var tree = get_tree()
+	tree.paused = !tree.paused
+	
+	if tree.paused:
+		$Pausable/Game.text = "Pause"
+	else:
+		$Pausable/Game.text = "Playing"
