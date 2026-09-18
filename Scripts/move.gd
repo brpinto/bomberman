672ac1d29d85adb2948f1fe224e8db1502db9157
Bruno Pinto <brpinto@student.42.fr>
2026-09-18 16:06:32 +0200

    add move resource

---
+++ Scripts/move.gd
@@ -0,0 +1,22 @@
+extends Resource
+class_name Move
+
+@export var speed: int = 50
+
+var animated_sprite: AnimatedSprite2D
+var target: CharacterBody2D
+
+func move_h(delta, direction):
+	animated_sprite.play("walk_side")
+	if direction == -1:
+		animated_sprite.flip_h = false
+	else:
+		animated_sprite.flip_h = true
+	target.position.x += direction * delta * speed
+
+func move_v(delta, direction):
+	if direction == -1:
+		animated_sprite.play("walk_up")
+	else:
+		animated_sprite.play("walk_down")
+	target.position.y += direction * delta * speed
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,27 +1,17 @@
 extends CharacterBody2D
+@export var move: Move
 
-
-const SPEED = 50.0
-const JUMP_VELOCITY = -400.0
+func _ready() -> void:
+	move.animated_sprite = $AnimatedSprite2D
+	move.target = self
 
 func _process(delta: float) -> void:
 	var direction: int
 	if Input.is_action_pressed("ui_left"):
-		$AnimatedSprite2D.play("walk_side")
-		$AnimatedSprite2D.flip_h = false
-		direction = - 1
-		position.x += direction * delta * SPEED
+		move.move_h(delta, -1)
 	if Input.is_action_pressed("ui_right"):
-		$AnimatedSprite2D.play("walk_side")
-		$AnimatedSprite2D.flip_h = true
-		direction = 1
-		position.x += direction * delta * SPEED
+		move.move_h(delta, 1)
 	if Input.is_action_pressed("ui_down"):
-		$AnimatedSprite2D.play("walk_down")
-		direction = 1
-		position.y += direction * delta * SPEED
+		move.move_v(delta, 1)
 	if Input.is_action_pressed("ui_up"):
-		$AnimatedSprite2D.play("walk_up")
-		direction = - 1
-		position.y += direction * delta * SPEED
-	
+		move.move_v(delta, -1)
