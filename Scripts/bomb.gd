aeb7254f0efe644f1333014d78c782fd6d4017ed
brpinto <brpinto@student.42.fr>
2026-09-23 23:11:25 +0200

    added explosion sprites

--- Scripts/bomb.gd
+++ Scripts/bomb.gd
@@ -1,11 +1,21 @@
 extends Area2D
 
 @export var bomb: Bomb
+var walls: TileMapLayer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimatedSprite2D.play("exploding")
+	bomb.level = get_tree().current_scene.get_node("Map")
+	walls = bomb.level.walls
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	var used_cells = walls.get_used_cells()
+	var pos = walls.local_to_map(global_position)
+	var surrounding = walls.get_surrounding_cells(pos)
+
+	for cell in surrounding:
+		for used in used_cells:
+			if cell == used:
+				print(cell)
+	
 func _process(delta: float) -> void:
 	pass
--- Scripts/bomb_res.gd
+++ Scripts/bomb_res.gd
@@ -2,6 +2,7 @@ class_name Bomb
 extends Resource
 
 var bomb = preload("res://Scenes/bomb.tscn")
+var level: TileMapLayer
 
 func _ready() -> void:
 	pass
